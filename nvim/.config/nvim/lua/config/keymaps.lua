005e1bc46547a6c31b0842071cfc026c3e1bdeef
jojimaaa <enzo.jojima@usp.br>
2026-08-31 21:25:30 -0300

    update: add vim-tmux-navigator, rename colorscheme.lua and change jj to jk

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = false })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = false })
 
